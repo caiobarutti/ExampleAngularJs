fc801923a46d4a1b648fb76b7b837c3bc4ef1440
Caio Barutti <caio.barutti@portaleducacao.com.br>
2014-08-13 13:58:01 -0400

    Adicionando About

--- ExampleAngularJs/app/app.js
+++ ExampleAngularJs/app/app.js
@@ -2,5 +2,6 @@
     .config(function ($routeProvider, $locationProvider) {
         $routeProvider.when('/Registration/Courses', { templateUrl: '/templates/Courses/courses.html', controller: 'CoursesController' });
         $routeProvider.when('/Registration/Students', { templateUrl: '/templates/Students/students.html', controller: 'StudentsController' });
+        $routeProvider.when('/Registration/About', { templateUrl: '/templates/Footer/about.html' });
         $locationProvider.html5Mode(true);
     });
